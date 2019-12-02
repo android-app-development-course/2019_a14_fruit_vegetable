049bad3808bfb2cd1798496098be602f7145293b
LINYUHAN1288 <1057000273@qq.com>
2019-12-02 22:37:47 +0800

    logo

---
+++ HealthyDiet8/app/src/main/java/com/zpffly/healthydiet8/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.zpffly.healthydiet8;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class SplashActivity extends AppCompatActivity{
+    private final int SPLASH_DISPLAY_LENGHT = 5000;
+    private Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+
+        handler = new Handler();
+        // 延迟SPLASH_DISPLAY_LENGHT时间然后跳转到MainActivity
+        handler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,
+                        MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGHT);
+
+    }
+}

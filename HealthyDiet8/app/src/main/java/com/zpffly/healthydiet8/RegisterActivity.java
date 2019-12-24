57d2f8030b5b4d48741cbb4082574d8643845bac
LINYUHAN1288 <1057000273@qq.com>
2019-12-24 12:57:07 +0800

    yuhan

--- HealthyDiet8/app/src/main/java/com/zpffly/healthydiet8/RegisterActivity.java
+++ HealthyDiet8/app/src/main/java/com/zpffly/healthydiet8/RegisterActivity.java
@@ -5,56 +5,54 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import top.wefor.circularanim.CircularAnim;
 
-public class loginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
-    private Button loginButton = null;
-    private FloatingActionButton registerButton = null;
+    private Button RegisterButton = null;
+    private FloatingActionButton LoginButton = null;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        loginButton = findViewById(R.id.tv_btn_login);
-        registerButton = findViewById(R.id.fab_register);
-        loginButton.setOnClickListener(clickListener);
-        registerButton.setOnClickListener(clickListener1);
+        setContentView(R.layout.activity_register);
+        RegisterButton = findViewById(R.id.tv_btn_register);
+        LoginButton = findViewById(R.id.fab_login);
+        RegisterButton.setOnClickListener(clickListener);
+        LoginButton.setOnClickListener(clickListener1);
+
     }
 
     private View.OnClickListener clickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            CircularAnim.fullActivity(loginActivity.this, loginButton)
+            CircularAnim.fullActivity(RegisterActivity.this, RegisterButton)
                     .colorOrImageRes(R.color.mycolor)
                     .go(new CircularAnim.OnAnimationEndListener(){
                         @Override
                         public void onAnimationEnd() {
-                            Intent loginIntent = new Intent(loginActivity.this, FragmentMeActivity.class);
-                            startActivity(loginIntent);
+                            Intent RegisterIntent = new Intent(RegisterActivity.this, loginActivity.class);
+                            startActivity(RegisterIntent);
                         }
                     });
-
         }
     };
-
     private View.OnClickListener clickListener1 = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            CircularAnim.fullActivity(loginActivity.this, registerButton)
+            CircularAnim.fullActivity(RegisterActivity.this, LoginButton)
                     .colorOrImageRes(R.color.mycolor)
                     .go(new CircularAnim.OnAnimationEndListener(){
                         @Override
                         public void onAnimationEnd() {
-                            Intent loginIntent = new Intent(loginActivity.this, RegisterActivity.class);
-                            startActivity(loginIntent);
+                            Intent RegisterIntent = new Intent(RegisterActivity.this, loginActivity.class);
+                            startActivity(RegisterIntent);
                         }
                     });
-
         }
     };
+
 
 }
